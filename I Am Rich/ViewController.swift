c7f9b630ab3b6d2924381e543b623d28edf6062c
cassiafranco <cassia.developer@gmail.com>
2021-04-23 18:29:22 -0300

    Initial Commit

---
+++ I Am Rich/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  I Am Rich
+//
+//  Created by Cassia Franco on 23/04/21.
+//  Copyright © 2021 Cassia Franco. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
